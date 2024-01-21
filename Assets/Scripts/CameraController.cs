a0355327168adf26e7b752d9be421fdfd65deac7
danish <danishalif16@gmail.com>
2024-01-21 12:15:32 +0700

    memperbaiki kamera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    Camera cam;
+    [SerializeField] float range;
     // Start is called before the first frame update
     void Start()
     {
+        cam = GetComponent<Camera>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var (center, size) = CalculateOrthoSize();
+        cam.transform.position = center;
+        cam.orthographicSize = size;
+    }
+
+    private (Vector3 center, float size) CalculateOrthoSize() {
+        var bounds = new Bounds();
+        foreach (var col in FindObjectsOfType<Collider2D>())
+        {
+            bounds.Encapsulate(col.bounds);
+        }
+
+        bounds.Expand(range);
+
+        var vertical = bounds.size.y * cam.pixelHeight / cam.pixelWidth;
+        var horizontal = bounds.size.x * cam.pixelHeight / cam.pixelWidth;
+
+        var size = Mathf.Max(horizontal, vertical) * 0.5f;
+        var center = bounds.center + new Vector3(0,0,-10);
+
+        return (center, size);
     }
 }
